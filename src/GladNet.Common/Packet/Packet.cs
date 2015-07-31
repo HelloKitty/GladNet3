20f269f15bdd2642dea450cb7c5abf20d76fbcff
Andrew Blakely <andrew.blakely@ymail.com>
2015-07-30 23:56:31 -0500

    Created Packets

---
+++ src/GladNet.Common/Packet/Packet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladNet.Common
+{
+	public class Packet
+	{
+		/// <summary>
+		/// Represents valid operation types for networked messages.
+		/// </summary>
+		public enum OperationType : byte
+		{
+			Event = 0,
+			Request = 1,
+			Response = 2
+		}
+
+		/// <summary>
+		/// Represents the potential delivery methods for packets to be sent.
+		/// </summary>
+		public enum DeliveryMethod
+		{
+			UnreliableAcceptDuplicate,
+			UnreliableDiscardStale,
+			ReliableUnordered,
+			ReliableDiscardStale,
+			ReliableOrdered
+		}
+
+		/// <summary>
+		/// Represents the result of trying to send a networked message.
+		/// </summary>
+		public enum SendResult : byte
+		{
+			FailedNotConnected = 0,
+			Sent = 1,
+			Queued = 2,
+			Dropped = 3,
+		}
+	}
+}
