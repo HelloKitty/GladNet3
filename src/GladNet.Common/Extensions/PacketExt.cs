2cea379f7dc5f3be5a2087deb6922db19d7b9a30
Andrew Blakely <andrew.blakely@ymail.com>
2015-07-31 00:09:31 -0500

    Added Lidgren reference to Project

--- src/GladNet.Common/Extensions/PacketExt.cs
+++ src/GladNet.Common/Extensions/PacketExt.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lidgren.Network;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace GladNet.Common.Extensions
 {
-	class PacketExt
+	public static class PacketExt
 	{
+		public static Packet.DeliveryMethod LidgrenDeliveryTypeToGladNetType(this NetDeliveryMethod lidgrenDeliveryMethod)
+		{
+			switch (lidgrenDeliveryMethod)
+			{
+				case NetDeliveryMethod.ReliableOrdered:
+					return Packet.DeliveryMethod.ReliableOrdered;
+				case NetDeliveryMethod.ReliableSequenced:
+					return Packet.DeliveryMethod.ReliableDiscardStale;
+				case NetDeliveryMethod.ReliableUnordered:
+					return Packet.DeliveryMethod.ReliableUnordered;
+				case NetDeliveryMethod.Unreliable:
+					return Packet.DeliveryMethod.UnreliableAcceptDuplicate;
+				case NetDeliveryMethod.UnreliableSequenced:
+					return Packet.DeliveryMethod.UnreliableDiscardStale;
+
+				default:
+					throw new ArgumentException("LidgrenDeliveryMethod invalid in " + typeof(PacketExt) + " unable to translate to GladNet type. Invalid code was: "
+						+ lidgrenDeliveryMethod.ToString(), "lidgrenDeliveryMethod");
+			}
+		}
 	}
 }
