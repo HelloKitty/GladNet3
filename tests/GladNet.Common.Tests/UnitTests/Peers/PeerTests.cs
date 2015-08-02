41779905c272c772d62a5916416897e4819a3e85
Andrew Blakely <andrew.blakely@ymail.com>
2015-08-02 11:58:53 -0500

    Seperated Peer receive tests

--- tests/GladNet.Common.Tests/UnitTests/Peers/PeerTests.cs
+++ tests/GladNet.Common.Tests/UnitTests/Peers/PeerTests.cs
@@ -19,7 +19,29 @@ namespace GladNet.Common.UnitTests
 		}
 
 		[Test]
-		public static void Test_Receive_Methods()
+		public static void Test_Request_Receive_Method()
+		{
+			//arrange
+			Mock<Peer> peer = new Mock<Peer>(MockBehavior.Loose);
+			//This sets it up so that the implementation of concrete methods are available.
+			peer.CallBase = true;
+
+			INetworkMessageReceiver receiver = peer.Object;
+			Mock<IMessageParameters> parameters = new Mock<IMessageParameters>(MockBehavior.Strict);
+
+			//Create the message types
+			Mock<IRequestMessage> requestMessage = new Mock<IRequestMessage>(MockBehavior.Strict);
+
+			//act
+			//Call for event.
+			receiver.OnNetworkMessageReceive(requestMessage.Object, parameters.Object);
+
+			//assert
+			peer.Protected().Verify("OnReceiveRequest", Times.Once(), requestMessage.Object, parameters.Object);
+		}
+
+		[Test]
+		public static void Test_Event_Receive_Method()
 		{
 			//arrange
 			Mock<Peer> peer = new Mock<Peer>(MockBehavior.Loose);
@@ -31,20 +53,37 @@ namespace GladNet.Common.UnitTests
 
 			//Create the message types
 			Mock<IEventMessage> eventMessage = new Mock<IEventMessage>(MockBehavior.Strict);
-			Mock<IResponseMessage> responseMessage = new Mock<IResponseMessage>(MockBehavior.Strict);
-			Mock<IRequestMessage> requestMessage = new Mock<IRequestMessage>(MockBehavior.Strict);
 
 			//act
 			//Call for event.
 			receiver.OnNetworkMessageReceive(eventMessage.Object, parameters.Object);
-			receiver.OnNetworkMessageReceive(responseMessage.Object, parameters.Object);
-			receiver.OnNetworkMessageReceive(requestMessage.Object, parameters.Object);
 
 			//assert
 			//peer.As<INetworkMessageReceiver>().Verify(p => p.OnNetworkMessageReceive(eventMessage.Object, parameters.Object), Times.Once());
 			peer.Protected().Verify("OnReceiveEvent", Times.Once(), eventMessage.Object, parameters.Object);
+		}
+
+		[Test]
+		public static void Test_Response_Receive_Method()
+		{
+			//arrange
+			Mock<Peer> peer = new Mock<Peer>(MockBehavior.Loose);
+			//This sets it up so that the implementation of concrete methods are available.
+			peer.CallBase = true;
+
+			INetworkMessageReceiver receiver = peer.Object;
+			Mock<IMessageParameters> parameters = new Mock<IMessageParameters>(MockBehavior.Strict);
+
+			//Create the message types
+			Mock<IResponseMessage> responseMessage = new Mock<IResponseMessage>(MockBehavior.Strict);
+
+			//act
+			//Call for event.
+			receiver.OnNetworkMessageReceive(responseMessage.Object, parameters.Object);
+
+			//assert
+			//peer.As<INetworkMessageReceiver>().Verify(p => p.OnNetworkMessageReceive(eventMessage.Object, parameters.Object), Times.Once());
 			peer.Protected().Verify("OnReceiveResponse", Times.Once(), responseMessage.Object, parameters.Object);
-			peer.Protected().Verify("OnReceiveRequest", Times.Once(), requestMessage.Object, parameters.Object);
 		}
 	}
 }
