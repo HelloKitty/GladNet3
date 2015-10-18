3db691c5bdf86df13c8f603ccad611edc2d87ecf
Andrew Blakely <andrew.blakely@ymail.com>
2015-10-18 16:42:31 -0500

    Trying to fix BD.sh

--- lib/BuildDependencies.sh
+++ lib/BuildDependencies.sh
@@ -1,10 +1,9 @@
+pwd
+
 xbuild ./LoggingServices/LoggingServices.sln /p:Configuration=Release /p:Platform="Any CPU"
-xcopy  /R /E /Y /q ".\LoggingServices\src\LoggingServices\bin\Release" ".\Dependency Builds\LoggingServices\DLLs\"
 
 xbuild ./Lidgren/Lidgren.Network.sln /p:Configuration=Release /p:Platform="Any CPU"
-xcopy  /R /E /Y /q ".\Lidgren\Lidgren.Network\bin\Release" ".\Dependency Builds\Lidgren\DLLs\"
 
 xbuild ./Net35Essentials/Net35Essentials.sln /p:Configuration=Release /p:Platform="Any CPU"
-xcopy  /R /E /Y /q ".\Net35Essentials\src\Net35Essentials\bin\Release" ".\Dependency Builds\Net35Essentials\DLLs\"
 
 PAUSE
