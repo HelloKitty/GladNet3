4c283f9a77e600a090089afc22d35e3d4e288dfe
Andrew Blakely <andrew.blakely@ymail.com>
2015-10-18 17:06:30 -0500

    Removed Pause in BD.sh; Fixed Travis cd

--- lib/BuildDependencies.sh
+++ lib/BuildDependencies.sh
@@ -3,5 +3,3 @@ xbuild ./LoggingServices/LoggingServices.sln /p:Configuration=Release /p:Platfor
 xbuild ./Lidgren/Lidgren.Network.sln /p:Configuration=Release /p:Platform="Any CPU"
 
 xbuild ./Net35Essentials/Net35Essentials.sln /p:Configuration=Release /p:Platform="Any CPU"
-
-PAUSE
