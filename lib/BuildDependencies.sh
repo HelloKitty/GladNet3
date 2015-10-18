905f9a53d61c89ceef5055ccd7b5764b3f01d054
Andrew Blakely <andrew.blakely@ymail.com>
2015-10-18 17:41:08 -0500

    Added BuildDepedency copying to .sh file

--- lib/BuildDependencies.sh
+++ lib/BuildDependencies.sh
@@ -1,5 +1,8 @@
 xbuild ./LoggingServices/LoggingServices.sln /p:Configuration=Release /p:Platform="Any CPU"
+cd --remove-destination /LoggingServices/src/LoggingServices/bin/Release /Dependency Builds/LoggingServices/DLLs/
 
 xbuild ./Lidgren/Lidgren.Network.sln /p:Configuration=Release /p:Platform="Any CPU"
+cd --remove-destination /Lidgren/Lidgren.Network/bin/Release /Dependency Builds/Lidgren/DLLs/
 
 xbuild ./Net35Essentials/Net35Essentials.sln /p:Configuration=Release /p:Platform="Any CPU"
+cd --remove-destination /Net35Essentials/src/Net35Essentials/bin/Release /Dependency Builds/Net35Essentials/DLLs/
