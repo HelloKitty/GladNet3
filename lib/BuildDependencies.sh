21c30ca03c1935a9d0c0da7a75a60b48d62c03f7
Andrew Blakely <andrew.blakely@ymail.com>
2015-10-18 17:54:57 -0500

    Fixed Path for cp

--- lib/BuildDependencies.sh
+++ lib/BuildDependencies.sh
@@ -1,8 +1,8 @@
 xbuild ./LoggingServices/LoggingServices.sln /p:Configuration=Release /p:Platform="Any CPU"
-cp -a --remove-destination /LoggingServices/src/LoggingServices/bin/Release/. /Dependency Builds/LoggingServices/DLLs
+cp -a --remove-destination ./LoggingServices/src/LoggingServices/bin/Release/. ./Dependency Builds/LoggingServices/DLLs/
 
 xbuild ./Lidgren/Lidgren.Network.sln /p:Configuration=Release /p:Platform="Any CPU"
-cp -a --remove-destination /Lidgren/Lidgren.Network/bin/Release/. /Dependency Builds/Lidgren/DLLs
+cp -a --remove-destination ./Lidgren/Lidgren.Network/bin/Release/. ./Dependency Builds/Lidgren/DLLs/
 
 xbuild ./Net35Essentials/Net35Essentials.sln /p:Configuration=Release /p:Platform="Any CPU"
-cp -a --remove-destination /Net35Essentials/src/Net35Essentials/bin/Release/. /Dependency Builds/Net35Essentials/DLLs
+cp -a --remove-destination ./Net35Essentials/src/Net35Essentials/bin/Release/. ./Dependency Builds/Net35Essentials/DLLs/
