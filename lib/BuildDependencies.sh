8ebc40837038dddda8c184255f03fb1611d612ee
Andrew Blakely <andrew.blakely@ymail.com>
2015-10-18 20:43:31 -0500

    Remove ls Debug

--- lib/BuildDependencies.sh
+++ lib/BuildDependencies.sh
@@ -9,5 +9,3 @@ rsync -avv ./Lidgren/Lidgren.Network/bin/Release/ Dependency\ Builds/Lidgren/DLL
 xbuild ./Net35Essentials/Net35Essentials.sln /p:Configuration=Release /p:Platform="Any CPU"
 mkdir -p Dependency\ Builds/Net35Essentials/DLLs/
 rsync -avv ./Net35Essentials/src/Net35Essentials/bin/Release/ Dependency\ Builds/Net35Essentials/DLLs/
-
-ls -R
