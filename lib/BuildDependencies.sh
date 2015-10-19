bf6e327ea21cd5a076ba2620415cc5bcb0800bec
Andrew Blakely <andrew.blakely@ymail.com>
2015-10-18 19:28:39 -0500

    Update RSync Command

--- lib/BuildDependencies.sh
+++ lib/BuildDependencies.sh
@@ -1,8 +1,8 @@
 xbuild ./LoggingServices/LoggingServices.sln /p:Configuration=Release /p:Platform="Any CPU"
-rsync -a ./LoggingServices/src/LoggingServices/bin/Release/ Dependency\ Builds/LoggingServices/DLLs/
+rsync -avR /LoggingServices/src/LoggingServices/bin/Release/ /Dependency\ Builds/LoggingServices/DLLs/
 
 xbuild ./Lidgren/Lidgren.Network.sln /p:Configuration=Release /p:Platform="Any CPU"
-rsync -a ./Lidgren/Lidgren.Network/bin/Release/ Dependency\ Builds/Lidgren/DLLs/
+rsync -avR /Lidgren/Lidgren.Network/bin/Release/ /Dependency\ Builds/Lidgren/DLLs/
 
 xbuild ./Net35Essentials/Net35Essentials.sln /p:Configuration=Release /p:Platform="Any CPU"
-rsync -a ./Net35Essentials/src/Net35Essentials/bin/Release/ Dependency\ Builds/Net35Essentials/DLLs/
+rsync -avR /Net35Essentials/src/Net35Essentials/bin/Release/ /Dependency\ Builds/Net35Essentials/DLLs/
