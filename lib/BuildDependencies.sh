95e9cc0360821448dc231948f4d00fbbacb0ed79
Andrew Blakely <andrew.blakely@ymail.com>
2015-10-18 20:27:09 -0500

    Removed Recurive rsync

--- lib/BuildDependencies.sh
+++ lib/BuildDependencies.sh
@@ -1,13 +1,13 @@
 xbuild ./LoggingServices/LoggingServices.sln /p:Configuration=Release /p:Platform="Any CPU"
 mkdir -p Dependency\ Builds/LoggingServices/DLLs/
-rsync -avvRr ./LoggingServices/src/LoggingServices/bin/Release/ Dependency\ Builds/LoggingServices/DLLs/
+rsync -avvR ./LoggingServices/src/LoggingServices/bin/Release/ Dependency\ Builds/LoggingServices/DLLs/
 
 xbuild ./Lidgren/Lidgren.Network.sln /p:Configuration=Release /p:Platform="Any CPU"
 mkdir -p Dependency\ Builds/Lidgren/DLLs/
-rsync -avvRr ./Lidgren/Lidgren.Network/bin/Release/ Dependency\ Builds/Lidgren/DLLs/
+rsync -avvR ./Lidgren/Lidgren.Network/bin/Release/ Dependency\ Builds/Lidgren/DLLs/
 
 xbuild ./Net35Essentials/Net35Essentials.sln /p:Configuration=Release /p:Platform="Any CPU"
 mkdir -p Dependency\ Builds/Net35Essentials/DLLs/
-rsync -avvRr ./Net35Essentials/src/Net35Essentials/bin/Release/ Dependency\ Builds/Net35Essentials/DLLs/
+rsync -avvR ./Net35Essentials/src/Net35Essentials/bin/Release/ Dependency\ Builds/Net35Essentials/DLLs/
 
 ls -R
