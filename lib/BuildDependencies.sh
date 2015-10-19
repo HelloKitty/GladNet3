f752ad47472d246090fc2aaace6a0adcbecef751
Andrew Blakely <andrew.blakely@ymail.com>
2015-10-18 20:01:09 -0500

    Fixed RSync targeted dir

--- lib/BuildDependencies.sh
+++ lib/BuildDependencies.sh
@@ -1,11 +1,11 @@
 xbuild ./LoggingServices/LoggingServices.sln /p:Configuration=Release /p:Platform="Any CPU"
 mkdir -p Dependency\ Builds/LoggingServices/DLLs/
-rsync -avR ./LoggingServices/src/LoggingServices/bin/Release Dependency\ Builds/LoggingServices/DLLs/
+rsync -avRr ./LoggingServices/src/LoggingServices/bin/Release/ Dependency\ Builds/LoggingServices/DLLs/
 
 xbuild ./Lidgren/Lidgren.Network.sln /p:Configuration=Release /p:Platform="Any CPU"
 mkdir -p Dependency\ Builds/Lidgren/DLLs/
-rsync -avR ./Lidgren/Lidgren.Network/bin/Release Dependency\ Builds/Lidgren/DLLs/
+rsync -avRr ./Lidgren/Lidgren.Network/bin/Release/ Dependency\ Builds/Lidgren/DLLs/
 
 xbuild ./Net35Essentials/Net35Essentials.sln /p:Configuration=Release /p:Platform="Any CPU"
 mkdir -p Dependency\ Builds/Net35Essentials/DLLs/
-rsync -avR ./Net35Essentials/src/Net35Essentials/bin/Release Dependency\ Builds/Net35Essentials/DLLs/
+rsync -avRr ./Net35Essentials/src/Net35Essentials/bin/Release/ Dependency\ Builds/Net35Essentials/DLLs/
