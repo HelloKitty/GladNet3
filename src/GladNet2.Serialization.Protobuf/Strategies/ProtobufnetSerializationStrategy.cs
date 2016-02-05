0d4e4c706b394d0a2ade812357a86f828852b469
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-05 16:43:41 -0600

    Renaming

---
+++ src/GladNet2.Serialization.Protobuf/Strategies/ProtobufnetSerializationStrategy.cs
@@ -0,0 +1,13 @@
+﻿using GladNet.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladNet.Serialization
+{
+	public class ProtobufnetSerializationStrategy : ISerializerStrategy
+	{
+
+	}
+}
