2247dcd28926ed59114d5f42e371bbb80ddf6a18
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-05 16:54:50 -0600

    Fixed Nuget packages

--- src/GladNet.Serializer.Protobuf/Strategies/ProtobufnetSerializationStrategy.cs
+++ src/GladNet.Serializer.Protobuf/Strategies/ProtobufnetSerializationStrategy.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace GladNet.Serialization
 {
-	public class ProtobufnetSerializationStrategy : ISerializerStrategy
+	public class ProtobufnetSerializationStrategy// : ISerializerStrategy
 	{
 
 	}
