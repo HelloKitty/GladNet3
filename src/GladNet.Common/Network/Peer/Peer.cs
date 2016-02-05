05463364468de3cec8b8e7d45f4634f781607410
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-04 22:39:31 -0600

    Remove Dead Code in Peer

--- src/GladNet.Common/Network/Peer/Peer.cs
+++ src/GladNet.Common/Network/Peer/Peer.cs
@@ -9,6 +9,10 @@ namespace GladNet.Common
 {
 	public abstract class Peer : INetPeer, IClassLogger
 	{
+		/// <summary>
+		/// Peer's service for sending network messages.
+		/// Use Extension methods or specific types for an neater API.
+		/// </summary>
 		public INetworkMessageSender NetworkSendService { get; private set; }
 
 		/// <summary>
@@ -28,33 +32,6 @@ namespace GladNet.Common
 			NetworkSendService = messageSender;
 			Logger = logger;
 		}
-
-		/*#region Message Senders
-		//The idea here is we return invalids because sometimes a Peer can't send a certain message type.
-		//In most cases external classes shouldn't be interfacing with this class in this fashion.
-		//They should instead used more explict send methods. However, this exists to allow for
-		//users to loosely couple their senders as best they can though they really shouldn't since
-		//it can't be known if the runetime Peer type offers that functionality.
-		[SuppressMessage("Microsoft.Design", "CA1026:DefaultParametersShouldNotBeUsed")]
-		public virtual SendResult TrySendMessage(OperationType opType, PacketPayload payload, DeliveryMethod deliveryMethod, bool encrypt = false, byte channel = 0)
-		{
-			payload.ThrowIfNull(nameof(payload));
-
-			//TODO: Implement logging.
-			if (!CanSend(opType))
-				return SendResult.Invalid;
-
-			return NetworkSendService.TrySendMessage(opType, payload, deliveryMethod, encrypt, channel); //ncrunch: no coverage Reason: The line doesn't have to be tested. This is abstract and can be overidden.
-		}
-
-		//This is non-virtual because it should mirror non-generic methods functionality. It makes no sense to change them individually.
-		public SendResult TrySendMessage<TPacketType>(OperationType opType, TPacketType payload) where TPacketType : PacketPayload, IStaticPayloadParameters
-		{
-			payload.ThrowIfNull(nameof(payload));
-
-			return TrySendMessage(opType, payload, payload.DeliveryMethod, payload.Encrypted, payload.Channel);
-		}
-		#endregion*/
 
 		protected abstract void OnStatusChanged(NetStatus status);
 
