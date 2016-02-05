5c0f69edd1924b30b6ddcb440212bf265d9344c1
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-05 17:51:09 -0600

    Added protobuf-net strats

---
+++ src/GladNet.Serializer.Protobuf/Strategies/ProtobufnetDeserializerStrategy.cs
@@ -0,0 +1,24 @@
+﻿using GladNet.Serializer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GladNet.Serialization
+{
+	public class ProtobufnetSerializationStrategy : IDeserializerStrategy
+	{
+		public TData Deserialize<TData>(byte[] value)
+		{
+			using (MemoryStream ms = new MemoryStream(value))
+			{
+				TData data = ProtoBuf.Serializer.Deserialize<TData>(ms);
+
+				data.ThrowIfNull(nameof(data));
+
+				return data;
+            }
+		}
+	}
+}
---
+++ src/GladNet.Serializer.Protobuf/Strategies/ProtobufnetSerializerStrategy.cs
@@ -0,0 +1,24 @@
+﻿using GladNet.Serializer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GladNet.Serialization
+{
+	public class ProtobufnetSerializerStrategy : ISerializerStrategy
+	{
+		public byte[] Serialize<TData>(TData data)
+		{
+			data.ThrowIfNull(nameof(data));
+
+			using (MemoryStream ms = new MemoryStream())
+			{
+				ProtoBuf.Serializer.Serialize(ms, data);
+
+				return ms.ToArray();
+			}
+		}
+	}
+}
