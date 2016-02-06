a1df9abe78746ce3e89635e2f6c10883117c402a
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-06 14:38:54 -0600

    ProtobufRegistry and Tests

---
+++ src/GladNet.Serializer.Protobuf/Strategies/ProtobufnetRegistry.cs
@@ -0,0 +1,52 @@
+﻿using ProtoBuf.Meta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Fasterflect;
+using System.Reflection;
+
+namespace GladNet.Serializer.Protobuf
+{
+	public class ProtobufnetRegistry : ISerializerRegistry
+	{
+		public bool Register(Type typeToRegister)
+		{
+			//Ok so here the fun begins.
+			//We need a recursive algorithm for walking the graph of the Type to register each
+			//serializable type. So, for every member we must look down into that Type recursively and register it.
+			//However, we must make sure to check if it's already been registered or we might get circular graphs which
+			//would overflow. But that'd be a weird type anyway.
+
+			typeToRegister.ThrowIfNull(nameof(typeToRegister));
+
+			if (RuntimeTypeModel.Default.IsDefined(typeToRegister))
+				return true;
+
+			//If it's not defined we need to add it.
+			if (typeToRegister.Attribute<GladNetSerializationContractAttribute>() == null)
+				return false;
+
+			MetaType typeModel = RuntimeTypeModel.Default.Add(typeToRegister, false);
+
+			//Add each member
+			foreach (MemberInfo mi in typeToRegister.MembersWith<GladNetMemberAttribute>(MemberTypes.Field | MemberTypes.Property, Flags.InstanceAnyVisibility))
+			{
+				typeModel.Add(mi.Attribute<GladNetMemberAttribute>().TagID, mi.Name);
+
+				//Now we might need to register this type aswell.
+				//Recur to try to register this type
+				Register(mi.Type());
+			}
+
+			//If might have a include on it so we should check it to register it with the subtype
+
+			GladNetSerializationIncludeAttribute include = typeToRegister.Attribute<GladNetSerializationIncludeAttribute>();
+
+			if (include != null)
+				typeModel.AddSubType(include.TagID, include.DerivedTypeToInclude);
+
+			return true;
+		}
+	}
+}
---
+++ tests/GladNet.Serializer.Protobuf.Tests/ProtobufnetRegistryTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladNet.Serializer.Protobuf.Tests
+{
+	[TestFixture]
+	public static class ProtobufnetRegistryTests
+	{
+		[Test]
+		public static void Test_Ctor_Doesnt_Throw()
+		{
+			//assert
+			Assert.DoesNotThrow(() => new ProtobufnetRegistry());
+		}
+
+		[Test]
+		public static void Test_Register_Throws_On_Null_Type()
+		{
+			//arrange
+			ProtobufnetRegistry registry = new ProtobufnetRegistry();
+
+			//assert
+			Assert.Throws<ArgumentNullException>(() => registry.Register(null));
+		}
+
+		[Test]
+		[TestCase(typeof(string))]
+		[TestCase(typeof(int))]
+		[TestCase(typeof(float))]
+		[TestCase(typeof(double))]
+		[TestCase(typeof(DateTime))]
+		public static void Test_Expected_Preregistered_Types_Indicate_They_Arent_Registered_And_Return_False(Type t)
+		{
+			//arrange
+			ProtobufnetRegistry registry = new ProtobufnetRegistry();
+
+			Assert.False(registry.Register(t));
+		}
+	}
+}
