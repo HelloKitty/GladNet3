c56e3d700aba807f2a1f07f7049b2b494658096b
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-05 23:42:05 -0600

    Add ISerializerRegistery

---
+++ src/GladNet.Serializer/Interfaces/ISerializerRegistry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladNet.Serializer
+{
+	/// <summary>
+	/// Implementer can register <see cref="Type"/>s for serialization.
+	/// </summary>
+	public interface ISerializerRegistry
+	{
+		bool Register(Type typeToRegister);
+	}
+}
