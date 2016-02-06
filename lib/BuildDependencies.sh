45abe5f8137db4031c6b27336849641111a49240
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-05 18:50:22 -0600

    Fixing package locating in travis (hopefully)

--- lib/BuildDependencies.sh
+++ lib/BuildDependencies.sh
@@ -6,13 +6,15 @@ chmod +x ./GladNet2/lib/BuildDependencies.sh
 cd GladNet2/lib/
 ./BuildDependencies.sh
 cd ..
+xbuild GladNetV2.sln /p:Configuration=Release /p:Platform="Any CPU"
 cd ..
 
-xbuild ./GladNet2/GladNetV2.sln /p:Configuration=Release /p:Platform="Any CPU"
 mkdir -p Dependency\ Builds/GladNet/DLLs/
 rsync -avv ./GladNet2/src/GladNet.Common/bin/Release/ Dependency\ Builds/GladNet/DLLs/
 rsync -avv ./GladNet2/src/GladNet.Serializer/bin/Release/ Dependency\ Builds/GladNet/DLLs/
 rsync -avv ./GladNet2/src/GladNet.Server.Common/bin/Release/ Dependency\ Builds/GladNet/DLLs/
+
+
 
 xbuild ./Net35Essentials/Net35Essentials.sln /p:Configuration=Release /p:Platform="Any CPU"
 mkdir -p Dependency\ Builds/Net35Essentials/DLLs/
