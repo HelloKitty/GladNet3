335de0f6c431f48e3989dd1a170328969c10a546
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-05 18:40:07 -0600

    Changed paths

--- lib/BuildDependencies.bat
+++ lib/BuildDependencies.bat
@@ -5,11 +5,6 @@ xcopy  /R /E /Y /q ".\LoggingServices\src\LoggingServices\bin\Release" ".\Depend
 xcopy  /R /E /Y /q ".\Net35Essentials\src\Net35Essentials\bin\Release" ".\Dependency Builds\Net35Essentials\DLLs\"
 
 nuget.exe restore .\GladNet2/GladNetV2.sln
-CD .\GladNet2\lib
-BuildDependencies.bat
-CD ..
-CD ..
-
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" .\GladNet2/GladNetV2.sln /p:Configuration=Release /p:Platform="Any CPU"
 xcopy  /R /E /Y /q ".\GladNet2\src\GladNet.Common\bin\Release" ".\Dependency Builds\GladNet\DLLs\"
 xcopy  /R /E /Y /q ".\GladNet2\src\GladNet.Serializer\bin\Release" ".\Dependency Builds\GladNet\DLLs\"
