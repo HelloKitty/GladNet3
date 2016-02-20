7ade33c0e527d0e459b5be2362bdf76942e703ba
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-20 01:31:59 -0600

    Fixed buildscript

--- lib/BuildDependencies.bat
+++ lib/BuildDependencies.bat
@@ -4,8 +4,15 @@ xcopy  /R /E /Y /q ".\LoggingServices\src\LoggingServices\bin\Release" ".\Depend
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" .\Net35Essentials/Net35Essentials.sln /p:Configuration=Release /p:Platform="Any CPU"
 xcopy  /R /E /Y /q ".\Net35Essentials\src\Net35Essentials\bin\Release" ".\Dependency Builds\Net35Essentials\DLLs\"
 
-nuget.exe restore .\GladNet2/GladNetV2.sln
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" .\GladNet2/GladNetV2.sln /p:Configuration=Release /p:Platform="Any CPU"
+nuget.exe restore GladNet2\GladNetV2.sln
+CD .\GladNet2\Lib
+call BuildDependencies.bat < nul
+CD ..
+
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" GladNetV2.sln /p:Configuration=Release /p:Platform="Any CPU"
+
+CD ..
+
 xcopy  /R /E /Y /q ".\GladNet2\src\GladNet.Common\bin\Release" ".\Dependency Builds\GladNet\DLLs\"
 xcopy  /R /E /Y /q ".\GladNet2\src\GladNet.Serializer\bin\Release" ".\Dependency Builds\GladNet\DLLs\"
 xcopy  /R /E /Y /q ".\GladNet2\src\GladNet.Server.Common\bin\Release" ".\Dependency Builds\GladNet\DLLs\"
