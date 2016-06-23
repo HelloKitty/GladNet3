93856c54151668340d9d80f723d0f0c477f34e54
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-23 00:54:43 -0500

    Refactored PacketPayload into seperate dll

--- tests/GladNet.Server.Common.Tests/UnitTests/Peers/ClientPeerSessionTests.cs
+++ tests/GladNet.Server.Common.Tests/UnitTests/Peers/ClientPeerSessionTests.cs
@@ -4,10 +4,6 @@ using Common.Logging;
 using Moq;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GladNet.Server.Common.Tests
 {
