1d3cbe6de784c9c345eecd0406f1ed7381b2fe0e
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-03 00:35:57 -0500

    Added PayloadContainer

---
+++ src/GladNet.Common/Network/Message/IPayloadContainer.cs
@@ -0,0 +1,19 @@
+﻿using GladNet.Payload;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladNet.Common
+{
+	/// <summary>
+	/// Implementer exposes a <see cref="PacketPayload"/>
+	/// </summary>
+	public interface IPayloadContainer
+	{
+		/// <summary>
+		/// Instance of the <see cref="PacketPayload"/> in the container.
+		/// </summary>
+		PacketPayload Payload { get; }
+	}
+}
