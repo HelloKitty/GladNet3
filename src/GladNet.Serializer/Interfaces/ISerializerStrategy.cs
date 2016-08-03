a02b66ae400ca1b0afbb53a04fbfdf8ca41d705c
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-03 16:46:26 -0500

    Added Stream Serialization overload

--- src/GladNet.Serializer/Interfaces/ISerializerStrategy.cs
+++ src/GladNet.Serializer/Interfaces/ISerializerStrategy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -17,5 +18,12 @@ namespace GladNet.Serializer
 		/// <param name="data">Instance/value to serialize.</param>
 		/// <returns>An instance/value serialized from the given instance of <typeparamref name="TData"/>.</returns>
 		byte[] Serialize<TData>(TData data);
+
+		/// <summary>
+		/// Serializes the a <typeparamref name="TData"/> instance/value to a <see cref="Stream"/>.
+		/// </summary>
+		/// <typeparam name="TData">Type being serialized.</typeparam>
+		/// <param name="data">Instance/value to serialize.</param>
+		void Serialize<TData>(Stream stream, TData data);
 	}
 }
