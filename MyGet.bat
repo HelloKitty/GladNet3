6a3c4eb66ab4a5bc26392c8e9f1a9a36c85a464a
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-09 17:41:29 -0500

    Added Release target for MSBUILD

--- MyGet.bat
+++ MyGet.bat
@@ -1,11 +1,11 @@
-msbuild src/GladNet.Common/GladNet.Common.csproj /p:DebugSymbols=False
-msbuild src/GladNet.Encryption/GladNet.Encryption.csproj /p:DebugSymbols=False
-msbuild src/GladNet.Engine.Common/GladNet.Engine.Common.csproj /p:DebugSymbols=False
-msbuild src/GladNet.Engine.Server/GladNet.Engine.Server.csproj /p:DebugSymbols=False
-msbuild src/GladNet.Message/GladNet.Message.csproj /p:DebugSymbols=False
-msbuild src/GladNet.Message.Handlers/GladNet.Message.Handlers.csproj /p:DebugSymbols=False
-msbuild src/GladNet.Payload/GladNet.Payload.csproj /p:DebugSymbols=False
-msbuild src/GladNet.Serializer/GladNet.Serializer.csproj /p:DebugSymbols=False
+msbuild src/GladNet.Common/GladNet.Common.csproj /p:Configuration=Release
+msbuild src/GladNet.Encryption/GladNet.Encryption.csproj /p:Configuration=Release
+msbuild src/GladNet.Engine.Common/GladNet.Engine.Common.csproj /p:Configuration=Release
+msbuild src/GladNet.Engine.Server/GladNet.Engine.Server.csproj /p:Configuration=Release
+msbuild src/GladNet.Message/GladNet.Message.csproj /p:Configuration=Release
+msbuild src/GladNet.Message.Handlers/GladNet.Message.Handlers.csproj /p:Configuration=Release
+msbuild src/GladNet.Payload/GladNet.Payload.csproj /p:Configuration=Release
+msbuild src/GladNet.Serializer/GladNet.Serializer.csproj /p:Configuration=Release
 
 cd ./src/GladNet.Payload.Authentication
 dotnet restore
