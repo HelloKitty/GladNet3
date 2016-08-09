882d65e88c9094035393076b8d43a93d61ae4e63
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-09 18:05:46 -0500

    Switched back to MSBUIlD for MyGet

--- MyGet.bat
+++ MyGet.bat
@@ -1,11 +1,11 @@
-%MsBuildExe% src/GladNet.Common/GladNet.Common.csproj /p:Configuration=Release
-%MsBuildExe% src/GladNet.Encryption/GladNet.Encryption.csproj /p:Configuration=Release
-%MsBuildExe% src/GladNet.Engine.Common/GladNet.Engine.Common.csproj /p:Configuration=Release
-%MsBuildExe% src/GladNet.Engine.Server/GladNet.Engine.Server.csproj /p:Configuration=Release
-%MsBuildExe% src/GladNet.Message/GladNet.Message.csproj /p:Configuration=Release
-%MsBuildExe% src/GladNet.Message.Handlers/GladNet.Message.Handlers.csproj /p:Configuration=Release
-%MsBuildExe% src/GladNet.Payload/GladNet.Payload.csproj /p:Configuration=Release
-%MsBuildExe% src/GladNet.Serializer/GladNet.Serializer.csproj /p:Configuration=Release
+msbuild ./src/GladNet.Common/GladNet.Common.csproj /p:Configuration=Release
+msbuild ./src/GladNet.Encryption/GladNet.Encryption.csproj /p:Configuration=Release
+msbuild ./src/GladNet.Engine.Common/GladNet.Engine.Common.csproj /p:Configuration=Release
+msbuild ./src/GladNet.Engine.Server/GladNet.Engine.Server.csproj /p:Configuration=Release
+msbuild ./src/GladNet.Message/GladNet.Message.csproj /p:Configuration=Release
+msbuild ./src/GladNet.Message.Handlers/GladNet.Message.Handlers.csproj /p:Configuration=Release
+msbuild ./src/GladNet.Payload/GladNet.Payload.csproj /p:Configuration=Release
+msbuild ./src/GladNet.Serializer/GladNet.Serializer.csproj /p:Configuration=Release
 
 cd ./src/GladNet.Payload.Authentication
 dotnet restore
