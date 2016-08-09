b346cb60beecf96568a49b76424fbde6a3146879
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-09 17:12:59 -0500

    Switch build order

--- MyGet.bat
+++ MyGet.bat
@@ -1,10 +1,10 @@
-dotnet pack src/GladNet.Payload.Authentication/ -c Release
+msbuild src/GladNet.Common/GladLive.Common.csproj /p:DebugSymbols=False
+msbuild src/GladNet.Encryption/GladNet.Encryption.csproj /p:DebugSymbols=False
+msbuild src/GladNet.Engine.Common/GladNet.Engine.Common.csproj /p:DebugSymbols=False
+msbuild src/GladNet.Engine.Server/GladNet.Engine.Server.csproj /p:DebugSymbols=False
+msbuild src/GladNet.Message/GladLive.Message.csproj /p:DebugSymbols=False
+msbuild src/GladNet.Message.Handlers/GladNet.Message.Handlers.csproj /p:DebugSymbols=False
+msbuild src/GladNet.Payload/GladLive.Payload.csproj /p:DebugSymbols=False
+msbuild src/GladNet.Serializer/GladNet.Serializer.csproj /p:DebugSymbols=False
 
-msbuild ./src/GladNet.Common/GladLive.Common.csproj /p:DebugSymbols=False
-msbuild ./src/GladNet.Encryption/GladNet.Encryption.csproj /p:DebugSymbols=False
-msbuild ./src/GladNet.Engine.Common/GladNet.Engine.Common.csproj /p:DebugSymbols=False
-msbuild ./src/GladNet.Engine.Server/GladNet.Engine.Server.csproj /p:DebugSymbols=False
-msbuild ./src/GladNet.Message/GladLive.Message.csproj /p:DebugSymbols=False
-msbuild ./src/GladNet.Message.Handlers/GladNet.Message.Handlers.csproj /p:DebugSymbols=False
-msbuild ./src/GladNet.Payload/GladLive.Payload.csproj /p:DebugSymbols=False
-msbuild ./src/GladNet.Serializer/GladNet.Serializer.csproj /p:DebugSymbols=False
+dotnet pack src/GladNet.Payload.Authentication/ -c Release
