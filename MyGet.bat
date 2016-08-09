b7f0e57dc0cbd7dfdc5ee722018d35a0737bc4f3
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-09 17:18:48 -0500

    Fixed Error in Naming

--- MyGet.bat
+++ MyGet.bat
@@ -1,10 +1,10 @@
-msbuild src/GladNet.Common/GladLive.Common.csproj /p:DebugSymbols=False
+msbuild src/GladNet.Common/GladNet.Common.csproj /p:DebugSymbols=False
 msbuild src/GladNet.Encryption/GladNet.Encryption.csproj /p:DebugSymbols=False
 msbuild src/GladNet.Engine.Common/GladNet.Engine.Common.csproj /p:DebugSymbols=False
 msbuild src/GladNet.Engine.Server/GladNet.Engine.Server.csproj /p:DebugSymbols=False
-msbuild src/GladNet.Message/GladLive.Message.csproj /p:DebugSymbols=False
+msbuild src/GladNet.Message/GladNet.Message.csproj /p:DebugSymbols=False
 msbuild src/GladNet.Message.Handlers/GladNet.Message.Handlers.csproj /p:DebugSymbols=False
-msbuild src/GladNet.Payload/GladLive.Payload.csproj /p:DebugSymbols=False
+msbuild src/GladNet.Payload/GladNet.Payload.csproj /p:DebugSymbols=False
 msbuild src/GladNet.Serializer/GladNet.Serializer.csproj /p:DebugSymbols=False
 
 cd ./src/GladNet.Payload.Authentication
