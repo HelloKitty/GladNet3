e8d63b49996909258e2cc9008c984bea15721faf
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-20 22:42:59 -0500

    Added Message Details Adapter

---
+++ src/GladNet.Lidgren.Engine.Common/Network/LidgrenMessageDetailsAdapter.cs
@@ -0,0 +1,28 @@
+﻿using GladNet.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GladNet.Common;
+using Lidgren.Network;
+using GladNet.Common.Extensions;
+
+namespace GladNet.Lidgren.Engine.Common
+{
+	public class LidgrenMessageDetailsAdapter : IMessageParameters
+	{
+		public byte Channel { get; }
+
+		public DeliveryMethod DeliveryMethod { get; }
+
+		public bool Encrypted { get; }
+
+		public LidgrenMessageDetailsAdapter(NetIncomingMessage message, bool wasEncrypted)
+		{
+			Channel = (byte)message.SequenceChannel;
+			DeliveryMethod = message.DeliveryMethod.ToGladNet();
+
+			Encrypted = wasEncrypted;
+		}
+	}
+}
