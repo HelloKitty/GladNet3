5f01643866d37d5f31beed619a827e7678fbaf34
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-21 16:21:25 -0500

    Created Message Contexts

---
+++ src/GladNet.Lidgren.Engine.Common/Network/Message/ContextMessages/Concrete/LidgrenNetworkMessageMessageContext.cs
@@ -0,0 +1,48 @@
+﻿using GladNet.Lidgren.Engine.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lidgren.Network;
+using GladNet.Message;
+using GladNet.Serializer;
+
+namespace GladNet.Lidgren.Engine.Common
+{
+	/// <summary>
+	/// Lidgren message context for <see cref="NetworkMessage"/>s.
+	/// </summary>
+	public class LidgrenNetworkMessageMessageContext : LidgrenMessageContext
+	{
+		/// <summary>
+		/// The generated message.
+		/// </summary>
+		public NetworkMessage GeneratedNetworkMessage { get; }
+
+		public LidgrenNetworkMessageMessageContext(NetIncomingMessage incomingMessage, IDeserializerStrategy deserializer)
+			: base(incomingMessage)
+		{
+			//The message.LengthBytes - message.PositionInBytes is from GladNet1
+			//We need to read the byte[] chunk that is from the current position to the end.
+			//For why we do this read this old exerp from GladNet2:
+			//"Due to message recycling we cannot trust the internal array of data to be of only the information that should be used for this package.
+			//We can trust the indicated size, not the length of .Data, and get a byte[] that represents the sent [Data].
+			//However, this will incur a GC penalty which may become an issue; more likely to be an issue on clients."
+			byte[] bytes = incomingMessage.ReadBytes(incomingMessage.LengthBytes - incomingMessage.PositionInBytes);
+
+			//Deserializer the network message and the payload.
+			GeneratedNetworkMessage = deserializer.Deserialize<NetworkMessage>(bytes);
+			GeneratedNetworkMessage?.Payload?.Deserialize(deserializer);
+		}
+
+		public override bool TryDispatch(INetworkMessageReceiver reciever)
+		{
+			if (GeneratedNetworkMessage == null)
+				return false;
+
+			GeneratedNetworkMessage.Dispatch(reciever, this); //pass the reciever and this (message parameters).
+
+			return true;
+		}
+	}
+}
---
+++ src/GladNet.Lidgren.Engine.Common/Network/Message/ContextMessages/Concrete/LidgrenStatusChangeMessageContext.cs
@@ -0,0 +1,38 @@
+﻿using GladNet.Lidgren.Engine.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lidgren.Network;
+using GladNet.Message;
+
+namespace GladNet.Lidgren.Engine.Common
+{
+	/// <summary>
+	/// Lidgren message context for <see cref="IStatusMessage"/>s.
+	/// </summary>
+	public class LidgrenStatusChangeMessageContext : LidgrenMessageContext
+	{
+		/// <summary>
+		/// The generated message.
+		/// </summary>
+		public IStatusMessage GeneratedStatusMessage { get; }
+
+		public LidgrenStatusChangeMessageContext(NetIncomingMessage incomingMessage) 
+			: base(incomingMessage)
+		{
+			//Generate a Lidgren status message for dispatching
+			GeneratedStatusMessage = new LidgrenStatusMessage((NetConnectionStatus)incomingMessage.ReadByte());
+		}
+
+		public override bool TryDispatch(INetworkMessageReceiver reciever)
+		{
+			if (GeneratedStatusMessage == null)
+				return false;
+
+			reciever.OnNetworkMessageReceive(GeneratedStatusMessage, this); //pass to the reciever the message and this (message parameters)
+
+			return true;
+		}
+	}
+}
