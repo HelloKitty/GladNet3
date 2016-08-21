b5966a51f1866e600a07f628a02d8ce9521cb50d
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-20 22:42:50 -0500

    Added Lidgren message dispatching

---
+++ src/GladNet.Lidgren.Engine.Common/Network/INetworkMessageDispatcher.cs
@@ -0,0 +1,20 @@
+﻿using Lidgren.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladNet.Lidgren.Engine.Common
+{
+	/// <summary>
+	/// Lidgren service contract for dispatching incoming network messages.
+	/// </summary>
+	public interface INetworkMessageDispatcher
+	{
+		/// <summary>
+		/// Dispatches the incoming message.
+		/// </summary>
+		/// <param name="message"></param>
+		void Dispatch(NetIncomingMessage message);
+	}
+}
