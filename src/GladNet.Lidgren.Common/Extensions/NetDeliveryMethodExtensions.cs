9ba0fd347fc11ea50ddacabf95716bf508b24f2c
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-20 20:54:57 -0500

    Added SendResult and Delivery extensions

--- src/GladNet.Lidgren.Common/Extensions/NetDeliveryMethodExtensions.cs
+++ src/GladNet.Lidgren.Common/Extensions/NetDeliveryMethodExtensions.cs
@@ -18,7 +18,51 @@ namespace GladNet.Common.Extensions
 		/// <param name="deliveryMethod">The instance to be used for translation.</param>
 		/// <exception cref="ArgumentException">Throws an exception if the <see cref="NetDeliveryMethod"/> is undefined.</exception>
 		/// <returns>The equivalent <see cref="NetDeliveryMethodExtensions.DeliveryMethod"/> for the given <see cref="NetDeliveryMethod"/></returns>
-		public static DeliveryMethod NetDeliveryMethodTypeToGladNetDeliveryType(this NetDeliveryMethod deliveryMethod)
+		public static NetDeliveryMethod ToLidgren(this DeliveryMethod deliveryMethod)
+		{
+			switch (deliveryMethod)
+			{
+				//Following comments are based on https://code.google.com/p/lidgren-network-gen3/wiki/Basics and https://github.com/lidgren/lidgren-network-gen3/blob/master/Lidgren.Network/NetDeliveryMethod.cs
+
+				//This delivery method guarantees that messages will always be received in the exact order they were sent.
+				case DeliveryMethod.ReliableOrdered:
+					return NetDeliveryMethod.ReliableOrdered;
+
+				//This delivery method is similar to UnreliableSequenced; except that it guarantees that SOME messages will be received - if you only send one message - it will be received. If you sent two messages quickly, and they get reordered in transit, only the newest message will be received 
+				//- but at least ONE of them will be received.
+				case DeliveryMethod.ReliableDiscardStale:
+					return NetDeliveryMethod.ReliableSequenced;
+
+				//This delivery method ensures that every message sent will be eventually received. 
+				//It does not however guarantee what order they will be received in; late messages may be delivered before newer ones.
+				case DeliveryMethod.ReliableUnordered:
+					return NetDeliveryMethod.ReliableUnordered;
+
+				//his is just UDP. Messages can be lost or received more than once. Messages may not be received in the same order as they were sent.
+				case DeliveryMethod.UnreliableAcceptDuplicate:
+					return  NetDeliveryMethod.Unreliable;
+
+				//Using this delivery method messages can still be lost; but you're protected against duplicated messages. 
+				//If a message arrives late; that is, if a message sent after this one has already been received - it will be dropped. 
+				//This means you will never receive "older" data than what you already have received.
+				case DeliveryMethod.UnreliableDiscardStale:
+					return NetDeliveryMethod.UnreliableSequenced;
+				
+				//This could occur if for some reason a value is passed that is an invalid delivery method.
+				//It is unlikely to occur. Could be done maliciously. Consult lidgren documentation in the event that this occurs in live servers.
+				default:
+					return NetDeliveryMethod.Unknown;
+			}
+		}
+
+		/// <summary>
+		/// This translates <see cref="NetDeliveryMethod"/> to GladNet <see cref="PacketPayload.DeliveryMethod"/>
+		/// Information related to this translation can be found here https://code.google.com/p/lidgren-network-gen3/wiki/Basics
+		/// </summary>
+		/// <param name="deliveryMethod">The instance to be used for translation.</param>
+		/// <exception cref="ArgumentException">Throws an exception if the <see cref="NetDeliveryMethod"/> is undefined.</exception>
+		/// <returns>The equivalent <see cref="NetDeliveryMethodExtensions.DeliveryMethod"/> for the given <see cref="NetDeliveryMethod"/></returns>
+		public static DeliveryMethod ToGladNet(this NetDeliveryMethod deliveryMethod)
 		{
 			switch (deliveryMethod)
 			{
@@ -47,7 +91,7 @@ namespace GladNet.Common.Extensions
 				//This means you will never receive "older" data than what you already have received.
 				case NetDeliveryMethod.UnreliableSequenced:
 					return DeliveryMethod.UnreliableDiscardStale;
-				
+
 				//This could occur if for some reason a value is passed that is an invalid delivery method.
 				//It is unlikely to occur. Could be done maliciously. Consult lidgren documentation in the event that this occurs in live servers.
 				default:
--- tests/GladNet.Lidgren.Tests/UnitTests/ExtensionMethods/DeliveryMethodExtTests.cs
+++ tests/GladNet.Lidgren.Tests/UnitTests/ExtensionMethods/DeliveryMethodExtTests.cs
@@ -21,7 +21,7 @@ namespace GladNet.Common.UnitTests
 		[TestCase(NetDeliveryMethod.UnreliableSequenced, DeliveryMethod.UnreliableDiscardStale)]
 		public static void Test_LidgrenDelivery_to_GladNetDelivery(NetDeliveryMethod lidgrenNetDeliveryMethod, DeliveryMethod gladnetDeliveryMethod)
 		{
-			Assert.AreEqual(lidgrenNetDeliveryMethod.NetDeliveryMethodTypeToGladNetDeliveryType(), gladnetDeliveryMethod);
+			Assert.AreEqual(gladnetDeliveryMethod, lidgrenNetDeliveryMethod.ToGladNet());
 		}
 
 		[Test]
@@ -33,7 +33,31 @@ namespace GladNet.Common.UnitTests
 			Assert.False(Enum.IsDefined(typeof(NetDeliveryMethod), test), "Can't use this constant for this test. Please change it.");
 
 			//assert
-			Assert.AreEqual(test.NetDeliveryMethodTypeToGladNetDeliveryType(), DeliveryMethod.Unknown);
+			Assert.AreEqual(test.ToGladNet(), DeliveryMethod.Unknown);
+		}
+
+		[Test]
+		//Test the mapping process. We DO NOT compile-time enforce mapping because we don't reference Lidgren dll where our DeliveryMethod is defined.
+		[TestCase(NetDeliveryMethod.ReliableOrdered, DeliveryMethod.ReliableOrdered)]
+		[TestCase(NetDeliveryMethod.ReliableSequenced, DeliveryMethod.ReliableDiscardStale)]
+		[TestCase(NetDeliveryMethod.ReliableUnordered, DeliveryMethod.ReliableUnordered)]
+		[TestCase(NetDeliveryMethod.Unreliable, DeliveryMethod.UnreliableAcceptDuplicate)]
+		[TestCase(NetDeliveryMethod.UnreliableSequenced, DeliveryMethod.UnreliableDiscardStale)]
+		public static void Test_GladNet_to_Lidgren(NetDeliveryMethod lidgrenNetDeliveryMethod, DeliveryMethod gladnetDeliveryMethod)
+		{
+			Assert.AreEqual(gladnetDeliveryMethod.ToLidgren(), lidgrenNetDeliveryMethod);
+		}
+
+		[Test]
+		public static void Test_GladNet_to_Lidgren_Invalid_DeliveryMethod()
+		{
+			//arrange
+			DeliveryMethod test = DeliveryMethod.Unknown + 50;
+
+			Assert.False(Enum.IsDefined(typeof(DeliveryMethod), test), "Can't use this constant for this test. Please change it.");
+
+			//assert
+			Assert.AreEqual(test.ToLidgren(), NetDeliveryMethod.Unknown);
 		}
 	}
 }
