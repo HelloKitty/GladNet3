dffcaa5acef01cfa88ad9254a6f101ece44efbc3
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-21 22:16:07 -0500

    Added Lidgren Unity Client

---
+++ src/GladNet.Lidgren.Client.Unity/ConnectionInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace GladNet.Lidgren.Client.Unity
+{
+	[Serializable]
+	public class ConnectionInfo
+	{
+		[SerializeField]
+		private string applicationIdentifier;
+
+		public string ApplicationIdentifier { get { return applicationIdentifier; } }
+
+		[SerializeField]
+		private int remotePort;
+
+		public int RemotePort { get { return remotePort; } }
+
+		[SerializeField]
+		private string serverIp;
+
+		public string ServerIp { get { return serverIp; } }
+	}
+}
