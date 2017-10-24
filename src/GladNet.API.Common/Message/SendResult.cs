37a02ebfb88520fa1abac572d039416fa857ddaf
HelloKitty <andrew.blakely@ymail.com>
2017-10-24 17:21:04 -0500

    Added SentResult enum

---
+++ src/GladNet.API.Common/Message/SendResult.cs
@@ -0,0 +1,12 @@
+﻿namespace GladNet
+{
+	/// <summary>
+	/// Enumeration of results of trying to send
+	/// a message.
+	/// </summary>
+	public enum SendResult
+	{
+		//TODO: Implement
+		Enqueued = 0,
+	}
+}
