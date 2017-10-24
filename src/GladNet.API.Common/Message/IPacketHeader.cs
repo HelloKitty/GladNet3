efc46a6ebc38607c534ad9298a1c9af73b8826e6
HelloKitty <andrew.blakely@ymail.com>
2017-10-24 18:00:24 -0500

    Added IPacketHeader

---
+++ src/GladNet.API.Common/Message/IPacketHeader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Proxy
+{
+	/// <summary>
+	/// Contract for packet headers.
+	/// </summary>
+	public interface IPacketHeader
+	{
+		/// <summary>
+		/// The size of the packet.
+		/// </summary>
+		int PacketSize { get; }
+
+		//The PacketSize contains the whole size of the packet
+		//So the payload is just the size minus the size of the packetsize field.
+		/// <summary>
+		/// Indicates the size of the payload.
+		/// </summary>
+		int PayloadSize { get; }
+	}
+}
