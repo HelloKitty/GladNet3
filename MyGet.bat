2ff33f918ad6126d99e540dc4d2e70d9fa6093c8
HelloKitty <andrew.blakely@ymail.com>
2017-10-24 17:06:20 -0500

    Remove GladNet2

--- MyGet.bat
+++
@@ -1,2 +0,0 @@
-%NUGET% restore GladNetV2.sln -NoCache -NonInteractive
-msbuild GladNetV2.sln /p:Configuration=Release
--- tests/RunTests.sh
+++
@@ -1,7 +0,0 @@
-mono ./testrunner/NUnit.Console.3.0.1/tools/nunit3-console.exe ./GladNet.Engine.Server.Tests/bin/Debug/GladNet.Engine.Server.Tests.dll
-mono ./testrunner/NUnit.Console.3.0.1/tools/nunit3-console.exe ./GladNet.Engine.Common.Tests/bin/Debug/GladNet.Engine.Common.Tests.dll
-mono ./testrunner/NUnit.Console.3.0.1/tools/nunit3-console.exe ./GladNet.Serializer.Tests/bin/Debug/GladNet.Serializer.Tests.dll
-mono ./testrunner/NUnit.Console.3.0.1/tools/nunit3-console.exe ./GladNet.Message.Tests/bin/Debug/GladNet.Message.Tests.dll
-mono ./testrunner/NUnit.Console.3.0.1/tools/nunit3-console.exe ./GladNet.Message.Handlers.Tests/bin/Debug/GladNet.Message.Handlers.Tests.dll
-mono ./testrunner/NUnit.Console.3.0.1/tools/nunit3-console.exe ./GladNet.Message.Handlers.Tests/bin/Debug/GladNet.Message.Handlers.Tests.dll
-mono ./testrunner/NUnit.Console.3.0.1/tools/nunit3-console.exe ./GladNet.Message.Handlers.Tests/bin/Debug/GladNet.Message.Handlers.Tests.dll
