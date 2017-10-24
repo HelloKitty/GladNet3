beec75090a77e4fe7839d5cd766707bf232e9676
HelloKitty <andrew.blakely@ymail.com>
2017-10-24 17:27:49 -0500

    Added IConnectable and extensions

---
+++ src/GladNet.API.Common/Extensions/IConnectableExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladNet
+{
+	public static class IConnectableExtensions
+	{
+		/// <summary>
+		/// Connects to the provided <see cref="address"/> with on the given <see cref="port"/>.
+		/// </summary>
+		/// <param name="connectable"></param>
+		/// <param name="address">The ip.</param>
+		/// <param name="port">The port.</param>
+		/// <returns>True if connection was successful.</returns>
+		public static bool Connect(this IConnectable connectable, IPAddress address, int port)
+		{
+			if(connectable == null) throw new ArgumentNullException(nameof(connectable));
+			if(address == null) throw new ArgumentNullException(nameof(address));
+			if(port < 0) throw new ArgumentOutOfRangeException(nameof(port));
+
+			return connectable.Connect(address.ToString(), port);
+		}
+
+		/// <summary>
+		/// Connects to the provided <see cref="address"/> with on the given <see cref="port"/>.
+		/// </summary>
+		/// <param name="connectable"></param>
+		/// <param name="address">The ip.</param>
+		/// <param name="port">The port.</param>
+		/// <returns>True if connection was successful.</returns>
+		public static async Task<bool> ConnectAsync(this IConnectable connectable, IPAddress address, int port)
+		{
+			if(connectable == null) throw new ArgumentNullException(nameof(connectable));
+			if(address == null) throw new ArgumentNullException(nameof(address));
+			if(port < 0) throw new ArgumentOutOfRangeException(nameof(port));
+
+			return await connectable.ConnectAsync(address.ToString(), port);
+		}
+	}
+}
---
+++ src/GladNet.API.Common/Network/IConnectable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladNet
+{
+	public interface IConnectable
+	{
+		/// <summary>
+		/// Connects to the provided <see cref="ip"/> with on the given <see cref="port"/>.
+		/// </summary>
+		/// <param name="ip">The ip.</param>
+		/// <param name="port">The port.</param>
+		/// <returns>True if connection was successful.</returns>
+		bool Connect(string ip, int port);
+
+		/// <summary>
+		/// Connects to the provided <see cref="ip"/> with on the given <see cref="port"/>.
+		/// </summary>
+		/// <param name="ip">The ip.</param>
+		/// <param name="port">The port.</param>
+		/// <returns>True if connection was successful.</returns>
+		Task<bool> ConnectAsync(string ip, int port);
+	}
+}
