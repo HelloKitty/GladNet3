64aa7f479d77efc152046aa8ad15c7c3ff123668
HelloKitty <andrew.blakely@ymail.com>
2017-10-25 12:02:42 -0500

    Simplified IBytesWrittable with extensions

--- src/GladNet.Common.API/Extensions/BytesReadWriteExtensions.cs
+++ src/GladNet.Common.API/Extensions/BytesReadWriteExtensions.cs
@@ -51,5 +51,45 @@ namespace GladNet
 
 			return await readable.ReadAsync(buffer, 0, count, new CancellationTokenSource(timeoutInMilliseconds).Token);
 		}
+
+		/// <summary>
+		/// Writes the provided <see cref="bytes"/>.
+		/// </summary>
+		/// <param name="writable"></param>
+		/// <param name="bytes">The bytes to write.</param>
+		public static void Write(this IBytesWrittable writable, byte[] bytes)
+		{
+			if(writable == null) throw new ArgumentNullException(nameof(writable));
+
+			writable.Write(bytes, 0, bytes.Length);
+		}
+
+		/// <summary>
+		/// Writes the provided <see cref="bytes"/> starting at the <see cref="offset"/>
+		/// for <see cref="count"/> many bytes.
+		/// </summary>
+		/// <param name="writable"></param>
+		/// <param name="bytes">The bytes to write.</param>
+		/// <param name="offset">The offset to start at.</param>
+		/// <param name="count">The amount of bytes to write.</param>
+		public static void Write(this IBytesWrittable writable, byte[] bytes, int offset, int count)
+		{
+			if(writable == null) throw new ArgumentNullException(nameof(writable));
+
+			writable.WriteAsync(bytes, offset, count);
+		}
+
+		/// <summary>
+		/// Writes the provided <see cref="bytes"/> asyncronously.
+		/// </summary>
+		/// <param name="writable"></param>
+		/// <param name="bytes">The bytes to write.</param>
+		/// <returns>An awaitable task.</returns>
+		public static async Task WriteAsync(this IBytesWrittable writable, byte[] bytes)
+		{
+			if(writable == null) throw new ArgumentNullException(nameof(writable));
+
+			await writable.WriteAsync(bytes, 0, bytes.Length);
+		}
 	}
 }
