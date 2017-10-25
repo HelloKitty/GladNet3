fd651de296f524e54d949d2bc99ecd95872b3b9b
HelloKitty <andrew.blakely@ymail.com>
2017-10-24 19:57:27 -0500

    Added DefaultClientMessageContext

---
+++ src/GladNet.Client/Models/DefaultClientMessageContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladNet
+{
+	/// <summary>
+	/// Default client message context.
+	/// Implements <see cref="IPeerMessageContext{TPayloadBaseType}"/>.
+	/// </summary>
+	/// <typeparam name="TPayloadBaseType">The type of the base payload.</typeparam>
+	public sealed class DefaultClientMessageContext<TPayloadBaseType> : IPeerMessageContext<TPayloadBaseType>
+		where TPayloadBaseType : class
+	{
+		/// <inheritdoc />
+		public IConnectionService ConnectionService { get; }
+
+		/// <inheritdoc />
+		public IPeerPayloadSendService<TPayloadBaseType> PayloadSendService { get; }
+
+		/// <inheritdoc />
+		public IPeerRequestSendService<TPayloadBaseType> RequestSendService { get; }
+
+		/// <inheritdoc />
+		public DefaultClientMessageContext(IConnectionService connectionService, IPeerPayloadSendService<TPayloadBaseType> payloadSendService, IPeerRequestSendService<TPayloadBaseType> requestSendService)
+		{
+			if(connectionService == null) throw new ArgumentNullException(nameof(connectionService));
+			if(payloadSendService == null) throw new ArgumentNullException(nameof(payloadSendService));
+			if(requestSendService == null) throw new ArgumentNullException(nameof(requestSendService));
+
+			ConnectionService = connectionService;
+			PayloadSendService = payloadSendService;
+			RequestSendService = requestSendService;
+		}
+	}
+}
