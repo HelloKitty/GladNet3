b27c4683c91835cb905022438efee2e3c7fc20cb
HelloKitty <andrew.blakely@ymail.com>
2017-10-25 20:29:42 -0500

    Changed Serializer API project name

--- src/GladNet.Serializer.API/INetworkSerializationService.cs
+++ src/GladNet.Serializer.API/INetworkSerializationService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace FreecraftCore.Serializer
+namespace GladNet
 {
 	public interface INetworkSerializationService
 	{
