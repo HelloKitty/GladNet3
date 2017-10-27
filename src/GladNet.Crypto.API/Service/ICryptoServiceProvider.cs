e025369e266c7f5a906b4d1a463130c5062f2565
HelloKitty <andrew.blakely@ymail.com>
2017-10-26 19:14:42 -0500

    Added blocksize prop to crypto service

--- src/GladNet.Crypto.API/Service/ICryptoServiceProvider.cs
+++ src/GladNet.Crypto.API/Service/ICryptoServiceProvider.cs
@@ -9,6 +9,14 @@ namespace GladNet
 	public interface ICryptoServiceProvider
 	{
 		/// <summary>
+		/// Indicates the size of the blocks that
+		/// must be a multiple of for crypting.
+		/// If 1 or 0 this means that no block size
+		/// is required.
+		/// </summary>
+		int BlockSize { get; }
+
+		/// <summary>
 		/// Passes the bytes inplace through the cipher.
 		/// Also returns them.
 		/// </summary>
@@ -22,6 +30,8 @@ namespace GladNet
 		/// Also returns them.
 		/// </summary>
 		/// <param name="bytes">The bytes to crypt.</param>
+		/// <param name="offset"></param>
+		/// <param name="count"></param>
 		/// <returns>The bytes.</returns>
 		byte[] Crypt(byte[] bytes, int offset, int count);
 	}
