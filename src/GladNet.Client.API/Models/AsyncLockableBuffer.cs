2b6814e4387e0263732f72b86e2b454f07d49a7b
HelloKitty <andrew.blakely@ymail.com>
2017-10-28 16:10:01 -0500

    Added LockableBuffer; Added Buffer.QuickUnsafeCopy

---
+++ src/GladNet.Client.API/Models/AsyncLockableBuffer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nito.AsyncEx;
+
+namespace GladNet
+{
+	/// <summary>
+	/// Combined buffer and locking object for syncronization.
+	/// </summary>
+	public sealed class AsyncLockableBuffer
+	{
+		/// <summary>
+		/// The async syncronization object.
+		/// </summary>
+		public AsyncLock BufferLock { get; } = new AsyncLock();
+
+		/// <summary>
+		/// The buffer.
+		/// </summary>
+		public byte[] Buffer { get; }
+
+		public AsyncLockableBuffer(int size)
+		{
+			if(size <= 0) throw new ArgumentOutOfRangeException(nameof(size));
+
+			Buffer = new byte[size];
+		}
+	}
+}
