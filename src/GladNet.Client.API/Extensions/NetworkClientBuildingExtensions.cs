189e0732e59e4a23ad356b8b61eccdf50e2f6eef
HelloKitty <andrew.blakely@ymail.com>
2017-10-28 16:35:23 -0500

    Added NetworkClient building extensions

---
+++ src/GladNet.Client.API/Extensions/NetworkClientBuildingExtensions.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+
+namespace GladNet
+{
+	public static class NetworkClientBuildingExtensions
+	{
+		/// <summary>
+		/// Creates a managed client adapter around the provided <see cref="client"/> providing a high level API
+		/// for consumption based on this simplified slimed down <see cref="IManagedNetworkClient{TPayloadWriteType,TPayloadReadType}"/>
+		/// interface.
+		/// </summary>
+		/// <typeparam name="TReadPayloadBaseType">The read type payload (inferred)</typeparam>
+		/// <typeparam name="TWritePayloadBaseType">The write type payload (inferred)</typeparam>
+		/// <param name="client">The client to adapt.</param>
+		/// <returns>A new managed client.</returns>
+		public static IManagedNetworkClient<TWritePayloadBaseType, TReadPayloadBaseType> AsManaged<TReadPayloadBaseType, TWritePayloadBaseType>(this INetworkMessageClient<TReadPayloadBaseType, TWritePayloadBaseType> client)
+			where TWritePayloadBaseType : class
+			where TReadPayloadBaseType : class
+		{
+			if(client == null) throw new ArgumentNullException(nameof(client));
+
+			//Adapt the provided network client to the managed network client interfaces.
+			return new ManagedNetworkClient<INetworkMessageClient<TReadPayloadBaseType, TWritePayloadBaseType>, TWritePayloadBaseType, TReadPayloadBaseType>(client);
+		}
+
+		/// <summary>
+		/// Creates a managed client adapter around the provided <see cref="client"/> providing a high level API
+		/// for consumption based on this simplified slimed down <see cref="IManagedNetworkClient{TPayloadWriteType,TPayloadReadType}"/>
+		/// interface.
+		/// </summary>
+		/// <typeparam name="TReadPayloadBaseType">The read type payload (inferred)</typeparam>
+		/// <typeparam name="TWritePayloadBaseType">The write type payload (inferred)</typeparam>
+		/// <param name="client">The client to adapt.</param>
+		/// <param name="logger"></param>
+		/// <returns>A new managed client.</returns>
+		public static IManagedNetworkClient<TWritePayloadBaseType, TReadPayloadBaseType> AsManaged<TReadPayloadBaseType, TWritePayloadBaseType>(this INetworkMessageClient<TReadPayloadBaseType, TWritePayloadBaseType> client, ILog logger)
+			where TWritePayloadBaseType : class
+			where TReadPayloadBaseType : class
+		{
+			if(client == null) throw new ArgumentNullException(nameof(client));
+			if(logger == null) throw new ArgumentNullException(nameof(logger));
+
+			//Adapt the provided network client to the managed network client interfaces.
+			return new ManagedNetworkClient<INetworkMessageClient<TReadPayloadBaseType, TWritePayloadBaseType>, TWritePayloadBaseType, TReadPayloadBaseType>(client, logger);
+		}
+
+		/// <summary>
+		/// Enables crypt handling for the client.
+		/// </summary>
+		/// <typeparam name="TNetworkClientType">The client type.</typeparam>
+		/// <param name="client">The client to add crypt handling to.</param>
+		/// <param name="encryptionService">The service used to encrypt.</param>
+		/// <param name="decryptionService">The service used to decrypt.</param>
+		/// <returns>A client with crypto handling functionality.</returns>
+		public static NetworkClientBase AddCryptHandling<TNetworkClientType>(this TNetworkClientType client, ICryptoServiceProvider encryptionService, ICryptoServiceProvider decryptionService)
+			where TNetworkClientType : NetworkClientBase
+		{
+			if(client == null) throw new ArgumentNullException(nameof(client));
+			if(encryptionService == null) throw new ArgumentNullException(nameof(encryptionService));
+			if(decryptionService == null) throw new ArgumentNullException(nameof(decryptionService));
+
+			//TODO: Check the blocksize for both. We don't support different blocksizes yet
+			if(encryptionService.BlockSize != 0 && encryptionService.BlockSize != 1)
+				return new NetworkClientFixedBlockSizeCryptoDecorator(client, encryptionService, decryptionService, encryptionService.BlockSize);
+
+			return new NetworkClientBlocklessCryptoDecorator(client, encryptionService, decryptionService);
+		}
+
+		/// <summary>
+		/// Enables header reading functionality to a client.
+		/// </summary>
+		/// <param name="client">The client to add header reading to.</param>
+		/// <returns>A client that can read headers from the network.</returns>
+		public static NetworkClientPacketHeaderReaderWriterDecorator<TPacketHeaderType> AddHeaderReading<TPacketHeaderType>(this NetworkClientBase client, INetworkSerializationService serializer, int headerSize) 
+			where TPacketHeaderType : IPacketHeader
+		{
+			if(client == null) throw new ArgumentNullException(nameof(client));
+			if(serializer == null) throw new ArgumentNullException(nameof(serializer));
+
+			return new NetworkClientPacketHeaderReaderWriterDecorator<TPacketHeaderType>(client, serializer, headerSize);
+		}
+
+		/// <summary>
+		/// Enables header reading functionality to a client.
+		/// </summary>
+		/// <typeparam name="TNetworkClientType">The client type.</typeparam>
+		/// <param name="client">The client to add header reading to.</param>
+		/// <returns>A client that can read headers from the network.</returns>
+		public static NetworkMessageClientBuilder<TNetworkClientType, TNetworkClientType> AddNetworkMessageReading<TNetworkClientType>(this TNetworkClientType client, INetworkSerializationService serializer)
+			where TNetworkClientType : NetworkClientBase, IPacketHeaderReadable, IPacketHeaderWritable
+		{
+			if(client == null) throw new ArgumentNullException(nameof(client));
+			if(serializer == null) throw new ArgumentNullException(nameof(serializer));
+
+			//New netmessage client builder
+			return new NetworkMessageClientBuilder<TNetworkClientType, TNetworkClientType>(serializer, client, client);
+		}
+	}
+}
---
+++ src/GladNet.Client.API/Extensions/NetworkMessageClientBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace GladNet
+{
+	/// <summary>
+	/// Builder for a network message client.
+	/// </summary>
+	/// <typeparam name="TNetworkType">The decorated network client type.</typeparam>
+	public sealed class NetworkMessageClientBuilder<TNetworkType, THeaderReaderWriterType>
+		where TNetworkType : NetworkClientBase
+		where THeaderReaderWriterType : IPacketHeaderReadable, IPacketHeaderWritable
+	{
+		/// <summary>
+		/// Serializer dependency for the net message client.
+		/// </summary>
+		public INetworkSerializationService Serializer { get; }
+
+		/// <summary>
+		/// The network client to decorate.
+		/// </summary>
+		public TNetworkType Client { get; }
+
+		public THeaderReaderWriterType HeaderReaderWriter { get; }
+
+		public NetworkMessageClientBuilder(INetworkSerializationService serializer, TNetworkType client, THeaderReaderWriterType headerReaderWriter)
+		{
+			if(serializer == null) throw new ArgumentNullException(nameof(serializer));
+			if(client == null) throw new ArgumentNullException(nameof(client));
+			if(headerReaderWriter == null) throw new ArgumentNullException(nameof(headerReaderWriter));
+
+			Serializer = serializer;
+			Client = client;
+			HeaderReaderWriter = headerReaderWriter;
+		}
+
+		//Hack: Use obsolete to warn user.
+		/// <summary>
+		/// Allows the fallback for not calling <see cref="For{TPayloadType}"/>.
+		/// </summary>
+		/// <param name="builder">The builder.</param>
+		[Obsolete("Did you mean to call For?")]
+		public static implicit operator TNetworkType(NetworkMessageClientBuilder<TNetworkType, THeaderReaderWriterType> builder)
+		{
+			if(builder == null) throw new ArgumentNullException(nameof(builder));
+
+			return builder.Client;
+		}
+
+		/// <summary>
+		/// Creates a <see cref="NetworkClientBase"/> client that can handle read and writing 
+		/// the specified generic <typeparamref name="TPayloadType"/>.
+		/// </summary>
+		/// <typeparam name="TPayloadType">The payload type.</typeparam>
+		/// <returns>A network message client.</returns>
+		public NetworkClientPacketPayloadReaderWriterDecorator<TNetworkType, THeaderReaderWriterType, TReadPayloadType, TWritePayloadType, TPayloadConstraintType> For<TReadPayloadType, TWritePayloadType, TPayloadConstraintType>(IPacketHeaderFactory<TPayloadConstraintType> packetHeaderFactory) 
+			where TReadPayloadType : class, TPayloadConstraintType 
+			where TWritePayloadType : class, TPayloadConstraintType
+		{
+			return new NetworkClientPacketPayloadReaderWriterDecorator<TNetworkType, THeaderReaderWriterType, TReadPayloadType, TWritePayloadType, TPayloadConstraintType>(Client, HeaderReaderWriter, Serializer, packetHeaderFactory);
+		}
+	}
+}
