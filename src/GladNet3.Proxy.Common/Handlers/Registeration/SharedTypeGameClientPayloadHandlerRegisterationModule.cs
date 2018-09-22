9099c0add31c58230b1d01329c61b7953d8070dd
HelloKitty <andrew.blakely@ymail.com>
2018-09-22 05:07:48 -0500

    Changed HandlerRegisteration
    This is a WIP for the Proxy. There are afew kinds of cases that require
    handling.

---
+++ src/GladNet3.Proxy.Common/Handlers/Registeration/SharedTypeGameClientPayloadHandlerRegisterationModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Autofac;
+using Autofac.Builder;
+using GladNet;
+
+namespace GladNet
+{
+	/// <summary>
+	/// Simplied Type alias for game registeration module for client handlers.
+	/// </summary>
+	public abstract class SharedTypeGameClientPayloadHandlerRegisterationModule<TBasePayloadType> : PayloadHandlerRegisterationModule<TBasePayloadType, TBasePayloadType, IProxiedMessageContext<TBasePayloadType, TBasePayloadType>> 
+		where TBasePayloadType : class 
+	{
+
+	}
+}
