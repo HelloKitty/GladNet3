85e542cbbe8b388bc6ff9d01f73375ef1d6a535d
HelloKitty <andrew.blakely@ymail.com>
2018-09-22 04:45:45 -0500

    Added default payload handlers for Proxy

---
+++ src/GladNet3.Proxy.Common/Handlers/BaseGameClientPayloadHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+using JetBrains.Annotations;
+
+namespace GladNet
+{
+	/// <summary>
+	/// Simplied type alias for game handlers that handle Client payloads sent from the client.
+	/// </summary>
+	[ClientPayloadHandler]
+	public abstract class BaseGameClientPayloadHandler<TSpecificPayloadType, TBasePayloadType> : BaseGamePayloadHandler<TSpecificPayloadType, TBasePayloadType>
+		where TSpecificPayloadType : class, TBasePayloadType
+		where TBasePayloadType : class, IPacketPayload
+	{
+		/// <inheritdoc />
+		protected BaseGameClientPayloadHandler([NotNull] ILog logger) 
+			: base(logger)
+		{
+
+		}
+	}
+}
---
+++ src/GladNet3.Proxy.Common/Handlers/BaseGameServerPayloadHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+using JetBrains.Annotations;
+
+namespace GladNet
+{
+	/// <summary>
+	/// Simplied type alias for game handlers that handle Server payloads sent from the server.
+	/// </summary>
+	[ServerPayloadHandler]
+	public abstract class BaseGameServerPayloadHandler<TSpecificPayloadType, TBasePayloadType> : BaseGamePayloadHandler<TSpecificPayloadType, TBasePayloadType> 
+		where TSpecificPayloadType : class, TBasePayloadType 
+		where TBasePayloadType : class, IPacketPayload
+	{
+		/// <inheritdoc />
+		protected BaseGameServerPayloadHandler([NotNull] ILog logger) 
+			: base(logger)
+		{
+
+		}
+	}
+}
