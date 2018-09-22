9f91cc50b65b54fbba39cf7fa4670b89ad7770b3
HelloKitty <andrew.blakely@ymail.com>
2018-09-22 03:27:22 -0500

    Added CommonLogging.ConsoleLogger from Freecraft
    Ported over from the FreecraftCore.Proxy project.

---
+++ src/GladNet.Proxy.Common/Logging/ConsoleLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using Common.Logging.Factory;
+
+namespace GladNet
+{
+	public sealed class ConsoleLogger : AbstractLogger
+	{
+		private LogLevel LoggingLevel { get; }
+
+		/// <inheritdoc />
+		public ConsoleLogger(LogLevel loggingLevel)
+		{
+			LoggingLevel = loggingLevel;
+		}
+
+		/// <inheritdoc />
+		protected override void WriteInternal(LogLevel level, object message, Exception exception)
+		{
+			Console.WriteLine($"{level}: {message} {exception?.Message}");
+		}
+
+		/// <inheritdoc />
+		public override bool IsTraceEnabled => LoggingLevel.HasFlag(LogLevel.Trace) || LoggingLevel == LogLevel.All;
+
+		/// <inheritdoc />
+		public override bool IsDebugEnabled => LoggingLevel.HasFlag(LogLevel.Debug) || LoggingLevel == LogLevel.All;
+
+		/// <inheritdoc />
+		public override bool IsErrorEnabled => LoggingLevel.HasFlag(LogLevel.Error) || LoggingLevel == LogLevel.All;
+
+		/// <inheritdoc />
+		public override bool IsFatalEnabled => LoggingLevel.HasFlag(LogLevel.Fatal) || LoggingLevel == LogLevel.All;
+
+		/// <inheritdoc />
+		public override bool IsInfoEnabled => LoggingLevel.HasFlag(LogLevel.Info) || LoggingLevel == LogLevel.All;
+
+		/// <inheritdoc />
+		public override bool IsWarnEnabled => LoggingLevel.HasFlag(LogLevel.Warn) || LoggingLevel == LogLevel.All;
+	}
+}
