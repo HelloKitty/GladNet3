2b43224be4ad1823054a231bcaf2c34700a30a2e
HelloKitty <andrew.blakely@ymail.com>
2018-09-22 03:28:22 -0500

    Added Server/Client payload handler attributes for Proxy

---
+++ src/GladNet.Proxy.Common/Attributes/ClientPayloadHandlerAttribute .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladNet
+{
+	/// <summary>
+	/// Attribute that hints that the handler is a client payload handler.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class, Inherited = true)]
+	public sealed class ClientPayloadHandlerAttribute : Attribute
+	{
+		
+	}
+}
---
+++ src/GladNet.Proxy.Common/Attributes/ServerPayloadHandlerAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladNet
+{
+	/// <summary>
+	/// Attribute that hints that the handler is a server payload handler.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class)]
+	public sealed class ServerPayloadHandlerAttribute : Attribute
+	{
+		
+	}
+}
