428de5cc5cfe9f2f68e1ac0094a76180feecc477
HelloKitty <andrew.blakely@ymail.com>
2018-12-10 17:16:06 -0600

    Added new network disconnection exception
    This can be used and caught internally during disconnection
    issues/races. We can spit out safer and cleaner results.

---
+++ src/GladNet3.Common.API/Exceptions/NetworkDisconnectedException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladNet
+{
+	/// <summary>
+	/// Disconnected network exception.
+	/// </summary>
+	public sealed class NetworkDisconnectedException : InvalidOperationException
+	{
+		/// <summary>
+		/// Creates a new disconnected exception that tells
+		/// up the stack that the network is disconnected and operations can't act on it.
+		/// </summary>
+		public NetworkDisconnectedException()
+			: base("The network is disconnected.")
+		{
+			
+		}
+
+		public NetworkDisconnectedException(string message)
+			: base(message)
+		{
+			
+		}
+	}
+}
