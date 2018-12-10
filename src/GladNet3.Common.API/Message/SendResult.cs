d3c7feba30c223491f2100ec696bff57ea1df6fe
HelloKitty <andrew.blakely@ymail.com>
2018-12-10 17:00:40 -0600

    Extended the SendResult enum

--- src/GladNet3.Common.API/Message/SendResult.cs
+++ src/GladNet3.Common.API/Message/SendResult.cs
@@ -7,8 +7,26 @@
 	public enum SendResult
 	{
 		//TODO: Implement
+		/// <summary>
+		/// Indicates that the message has been enqueued for sending.
+		/// The result can't be known, and would be only determined in the future.
+		/// </summary>
 		Enqueued = 0,
 
-		Sent = 1
+		/// <summary>
+		/// Indicates that the message has been sent.
+		/// </summary>
+		Sent = 1,
+
+		/// <summary>
+		/// Indicates that the network is disconnected
+		/// and the message cannot be sent.
+		/// </summary>
+		Disconnected = 2,
+
+		/// <summary>
+		/// Indicates an error was encountered.
+		/// </summary>
+		Error = 3
 	}
 }
