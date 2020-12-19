02e11e676e4dd3835325140ba47656fb39fe3586
HelloKitty <andrew.blakely@ymail.com>
2020-12-19 01:14:34 -0600

    Improved TCP pipeline advancing safety

--- src/GladNet.Server.DotNetTcpServer/Session/BaseTcpManagedSession.cs
+++ src/GladNet.Server.DotNetTcpServer/Session/BaseTcpManagedSession.cs
@@ -85,27 +85,46 @@ namespace GladNet
 
 					IPacketHeader header = ReadIncomingPacketHeader(Connection.Input, in result);
 
-					do
+					while (!token.IsCancellationRequested)
 					{
 						//Now with the header we know how much data we must now read for the payload.
 						result = await Connection.Input.ReadAsync(token);
 
 						//This call will also use the cancel token so we don't need to check it in the nested-loop.
-						if (!IsReadResultValid(in result))
+						if(!IsReadResultValid(in result))
 							return;
 
 						//This is dumb and hacky, but we need to know if we shall need to read again
 						//This means we're still at the START of the buffer (haven't read anything)
 						//but have technically aware/inspected to the END position.
-						if (result.Buffer.Length < header.PayloadSize)
+						if(result.Buffer.Length < header.PayloadSize)
 							Connection.Input.AdvanceTo(result.Buffer.Start, result.Buffer.End);
+						else
+							break;
+					}
 
-					} while (result.Buffer.Length < header.PayloadSize); //We need at least PAYLOAD SIZE bytes otherwise we cannot read the payload.
-
-					//TODO: Valid incoming packet lengths to avoid a stack overflow.
-					//This point we have a VALID read result that is NOT less than header.PayloadSize
-					//therefore it should be safe now to read the incoming packet.
-					TPayloadReadType message = ReadIncomingPacketPayload(Connection.Input, in result, header.PayloadSize);
+					TPayloadReadType message;
+					try
+					{
+						//TODO: Valid incoming packet lengths to avoid a stack overflow.
+						//This point we have a VALID read result that is NOT less than header.PayloadSize
+						//therefore it should be safe now to read the incoming packet.
+						message = ReadIncomingPacketPayload(in result, header.PayloadSize);
+					}
+					catch (Exception)
+					{
+						//TODO: We should log WHY but basically we should no longer continue the network listener.
+						return;
+					}
+					finally
+					{
+						//So serialization should output an offset read. However, we should not use
+						//that as the basis for the bytes read. Serialization can be WRONG and conflict with
+						//the packet header's defined size. Therefore, we should trust packet header over serialization
+						//logic ALWAYS and this advance should be in a finally block for sure.
+						Connection.Input.AdvanceTo(result.Buffer.GetPosition(header.PayloadSize));
+					}
+					
 					await OnNetworkMessageReceived(new NetworkIncomingMessage<TPayloadReadType>(header, message));
 				}
 			}
@@ -134,43 +153,46 @@ namespace GladNet
 			}
 		}
 
-		private unsafe TPayloadReadType ReadIncomingPacketPayload(PipeReader reader, in ReadResult result, int payloadSize)
+		private TPayloadReadType ReadIncomingPacketPayload(in ReadResult result, int payloadSize)
 		{
-			//I opted to do this instead of stack alloc because of HUGE dangers in stack alloc and this if pretty efficient
+			//I opted to do this instead of stack alloc because of HUGE dangers in stack alloc and this is pretty efficient
 			//buffer usage anyway.
 			byte[] rentedBuffer = ArrayPool<byte>.Shared.Rent(payloadSize);
 			Span<byte> buffer = new Span<byte>(rentedBuffer, 0, payloadSize);
 
 			try
 			{
+				//This copy is BAD but it really avoids a lot of API headaches
 				result.Buffer.Slice(0, payloadSize).CopyTo(buffer);
 
 				int offset = 0;
-				TPayloadReadType message = MessageServices.MessageDeserializer.Deserialize(buffer, ref offset);
-
-				//Steps the reader N bytes forward (if we don't do this WE LEAK AND WORSE!!)
-				reader.AdvanceTo(result.Buffer.GetPosition(offset));
-				return message;
+				return MessageServices.MessageDeserializer.Deserialize(buffer, ref offset);
 			}
 			finally
 			{
 				ArrayPool<byte>.Shared.Return(rentedBuffer);
 			}
-
 		}
 
 		private IPacketHeader ReadIncomingPacketHeader(PipeReader reader, in ReadResult result)
 		{
-			//The implementation MUST be that this can be trusted to be the EXACT size of binary data that will be read.
-			int exactHeaderByteCount = MessageServices.PacketHeaderFactory.ComputeHeaderSize(result.Buffer);
+			int exactHeaderByteCount = 0;
+			try
+			{
+				//The implementation MUST be that this can be trusted to be the EXACT size of binary data that will be read.
+				exactHeaderByteCount = MessageServices.PacketHeaderFactory.ComputeHeaderSize(result.Buffer);
 
-			IPacketHeader header;
-			using(var context = new PacketHeaderCreationContext(result.Buffer, exactHeaderByteCount))
-				header = MessageServices.PacketHeaderFactory.Create(context);
+				IPacketHeader header;
+				using(var context = new PacketHeaderCreationContext(result.Buffer, exactHeaderByteCount))
+					header = MessageServices.PacketHeaderFactory.Create(context);
 
-			//Steps the reader N bytes forward (if we don't do this WE LEAK AND WORSE!!)
-			reader.AdvanceTo(result.Buffer.GetPosition(exactHeaderByteCount));
-			return header;
+				return header;
+			}
+			finally
+			{
+				//Advance to only the exact header bytes read, consumed and examined. Do not use buffer lengths ever!
+				reader.AdvanceTo(result.Buffer.GetPosition(exactHeaderByteCount));
+			}
 		}
 
 		private bool IsReadResultValid(in ReadResult result)
