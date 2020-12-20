26ce0a0a3a2deb9aab343b15aa4c18e3c94dc892
HelloKitty <andrew.blakely@ymail.com>
2020-12-19 22:03:40 -0600

    Implemented AsyncExProducerConsumerQueueAsyncMessageQueue

---
+++ src/GladNet.API/Collections/AsyncExProducerConsumerQueueAsyncMessageQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+
+namespace GladNet
+{
+	/// <summary>
+	/// <see cref="AsyncProducerConsumerQueue{T}"/> implementation of the GladNet <see cref="IAsyncMessageQueue{TMessageType}"/>
+	/// </summary>
+	/// <typeparam name="TMessageType"></typeparam>
+	public sealed class AsyncExProducerConsumerQueueAsyncMessageQueue<TMessageType> 
+		: IAsyncMessageQueue<TMessageType>, IDisposable
+	{
+		private AsyncProducerConsumerQueue<TMessageType> InternalQueue { get; } = new AsyncProducerConsumerQueue<TMessageType>();
+
+		/// <inheritdoc />
+		public async Task<TMessageType> DequeueAsync(CancellationToken token = default)
+		{
+			return await InternalQueue.DequeueAsync(token);
+		}
+
+		/// <inheritdoc />
+		public async Task<bool> EnqueueAsync(TMessageType message, CancellationToken token = default)
+		{
+			await InternalQueue.EnqueueAsync(message, token);
+			return true;
+		}
+
+		public void Dispose()
+		{
+			InternalQueue.CompleteAdding();
+		}
+	}
+}
