0e69eac561986d89f93277cadeed866dc35a7c1c
HelloKitty <andrew.blakely@ymail.com>
2020-12-20 16:05:43 -0600

    Re-added the InPlaceNetworkMessageDispatchingStrategy

---
+++ src/GladNet.API.Server/Message/Dispatching/InPlaceNetworkMessageDispatchingStrategy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GladNet
+{
+	/// <summary>
+	/// The default in place dispatching strategy for network messages which
+	/// handles them immediately without any enqueueing and will yield back when
+	/// the message handling has been completed.
+	/// </summary>
+	/// <typeparam name="TPayloadWriteType">The type of payload that is writeable by the application..</typeparam>
+	/// <typeparam name="TPayloadReadType">The type of payload that is handled.</typeparam>
+	public sealed class InPlaceNetworkMessageDispatchingStrategy<TPayloadReadType, TPayloadWriteType> : INetworkMessageDispatchingStrategy<TPayloadReadType, TPayloadWriteType>
+		where TPayloadWriteType : class
+		where TPayloadReadType : class
+	{
+		/// <summary>
+		/// Handler service that can handle the incoming message.
+		/// </summary>
+		private IMessageHandlerService<TPayloadReadType, SessionMessageContext<TPayloadWriteType>> HandlerService { get; }
+
+		public InPlaceNetworkMessageDispatchingStrategy(IMessageHandlerService<TPayloadReadType, SessionMessageContext<TPayloadWriteType>> handlerService)
+		{
+			HandlerService = handlerService ?? throw new ArgumentNullException(nameof(handlerService));
+		}
+
+		/// <inheritdoc />
+		public async Task DispatchNetworkMessageAsync(SessionMessageContext<TPayloadWriteType> context, NetworkIncomingMessage<TPayloadReadType> message, CancellationToken token = default)
+		{
+			await HandlerService.HandleMessageAsync(context, message.Payload, token);
+		}
+	}
+}
