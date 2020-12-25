165068b9eb6a4289f9addead44bf30753f11b038
HelloKitty <andrew.blakely@ymail.com>
2020-12-24 22:29:23 -0600

    Added IDisposableAttachable

---
+++ src/GladNet.API/Network/IDisposableAttachable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladNet
+{
+	/// <summary>
+	/// Contract for types that can have <see cref="IDisposable"/>s attached to them.
+	/// </summary>
+	public interface IDisposableAttachable
+	{
+		/// <summary>
+		/// Attaches the disposable instance to the object.
+		/// </summary>
+		/// <param name="disposable">The disposable to attach.</param>
+		void AttachDisposable(IDisposable disposable);
+	}
+}
