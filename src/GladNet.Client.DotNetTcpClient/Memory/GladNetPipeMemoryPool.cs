c24747f88153a2a85b8fa7b862d59f2f8bb87c41
HelloKitty <andrew.blakely@ymail.com>
2022-09-19 11:22:56 -0500

    Fixed fault causing blocking on write threads due to shared array pool contention

---
+++ src/GladNet.Client.DotNetTcpClient/Memory/GladNetPipeMemoryPool.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace GladNet
+{
+	// Based on: https://github.com/dotnet/runtime/blob/77a714d26dc434012c8234447b9e1183ce7be4a6/src/libraries/System.Memory/src/System/Buffers/ArrayMemoryPool.cs
+	internal class GladNetPipeMemoryPool : MemoryPool<byte>
+	{
+		public sealed override int MaxBufferSize => int.MaxValue;
+
+		private ArrayPool<byte> SharedPipePool { get; } = ArrayPool<byte>.Create();
+
+		public sealed override IMemoryOwner<byte> Rent(int minimumBufferSize = -1)
+		{
+			if(minimumBufferSize == -1)
+				minimumBufferSize = 1 + (4095 / Unsafe.SizeOf<byte>());
+			else if(((uint)minimumBufferSize) > int.MaxValue)
+				throw new InvalidOperationException($"Provided buffer size: {minimumBufferSize} is too high.");
+
+			return new GladNetPipeMemoryPoolBuffer(minimumBufferSize, SharedPipePool);
+		}
+
+		protected sealed override void Dispose(bool disposing) { }  // ArrayMemoryPool is a shared pool so Dispose() would be a nop even if there were native resources to dispose.
+
+		// Based on: https://github.com/dotnet/runtime/blob/9768606ea1f0aa1be6098143ded330dadac8cf91/src/libraries/System.Memory/src/System/Buffers/ArrayMemoryPool.ArrayMemoryPoolBuffer.cs
+		private sealed class GladNetPipeMemoryPoolBuffer : IMemoryOwner<byte>, IDisposable
+		{
+			private ArrayPool<byte> Pool { get; }
+
+			private byte[] _array;
+
+			public GladNetPipeMemoryPoolBuffer(int size, ArrayPool<byte> pool)
+			{
+				Pool = pool ?? throw new ArgumentNullException(nameof(pool));
+				_array = Pool.Rent(size);
+			}
+
+			public Memory<byte> Memory
+			{
+				get
+				{
+					byte[] array = _array;
+
+					if (array is null)
+						throw new ObjectDisposedException("array");
+
+					return new Memory<byte>(array);
+				}
+			}
+
+			public void Dispose()
+			{
+				byte[] array = _array;
+				if(array != null)
+				{
+					_array = null;
+					Pool.Return(array);
+				}
+			}
+		}
+	}
+}
--- src/GladNet.Client.DotNetTcpClient/TCPSocketConnectionFactory.cs
+++ src/GladNet.Client.DotNetTcpClient/TCPSocketConnectionFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.IO.Pipelines;
 using System.Net;
@@ -22,10 +23,14 @@ namespace GladNet
 
 			Socket client = new Socket(SocketType.Stream, ProtocolType.Tcp);
 
+			// We cannot allow default memory pool usage, see: https://github.com/dotnet/runtime/blob/main/src/libraries/System.IO.Pipelines/src/System/IO/Pipelines/StreamPipeReader.cs#L596
+			// It can be seen that this will opt to use default array pool which will be TlsOverPerCoreLockedStacksArrayPool which can have significant contention
 			//Let the caller connect if they want to.
 			//await client.ConnectAsync(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6969));
+			PipeOptions sendOptions = new PipeOptions(new GladNetPipeMemoryPool());
+			PipeOptions receiveOptions = new PipeOptions(new GladNetPipeMemoryPool());
 
-			return SocketConnection.Create(client, PipeOptions.Default, PipeOptions.Default, SocketConnectionOptions.ZeroLengthReads);
+			return SocketConnection.Create(client, sendOptions, receiveOptions, SocketConnectionOptions.ZeroLengthReads);
 		}
 	}
 }
