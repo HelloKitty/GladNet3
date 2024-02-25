8a1545bad99a53c96bf20318da2fecaea8576055
HelloKitty <andrew.blakely@ymail.com>
2024-02-25 14:44:29 -0600

    Added CloseAsync to the WebSocketConnection

--- src/GladNet.API.WebSocket/Connection/IWebSocketConnection.cs
+++ src/GladNet.API.WebSocket/Connection/IWebSocketConnection.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace GladNet
 {
 	public interface IWebSocketConnection
 	{
-
+		/// <summary>
+		/// Closes the WebSocket connection as an asynchronous operation using the close handshake defined in the WebSocket protocol specification section 7.
+		/// </summary>
+		/// <param name="closeStatus">Indicates the reason for closing the WebSocket connection.</param>
+		/// <param name="statusDescription">Specifies a human readable explanation as to why the connection is closed.</param>
+		/// <param name="cancellationToken">The token that can be used to propagate notification that operations should be canceled.</param>
+		/// <returns>The task object representing the asynchronous operation.</returns>
+		Task CloseAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken = default);
 	}
 }
